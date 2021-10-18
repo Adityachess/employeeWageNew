7a3ee33dab6d20f0542a0c9bed773edd96c34426
Adityachess <adityadone9398@gmail.com>
2021-10-18 12:35:56 +0530

    Added:Check wage of employee till hundred hours.

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -12,7 +12,8 @@ public class EmployeeWage {
 	int employeeHour;
 	int daysPerMonth = 20;
 	int monthlyWage;
-
+	int totalHours = 100;
+	int totalDays = 20;
 
 	public void employeeAttdence() {
 		int empCheck = (int) (Math.random() * 10) % 2;
@@ -34,7 +35,7 @@ public class EmployeeWage {
 		partTimeWage = wagePerHours * partTimeHours;
 		System.out.println("Employee Part Time Wage :" + partTimeWage);
 	}
-	
+
 	public void bySwitchCase() {
 		Random ran = new Random();
 		int empCheck = ran.nextInt(3);
@@ -54,13 +55,45 @@ public class EmployeeWage {
 		}
 		System.out.println();
 	}
-	
+
 	public void monthlyWage() {
 
 		monthlyWage = dailyWage * daysPerMonth;
 		System.out.println("Monthly Wage Of Employee is =" + monthlyWage);
 	}
 
+	public void calculateWagesTillGivenCond() {
+
+		int empWage = 0;
+		int totalEmpWage = 0;
+		int totalWorkingDays = 0;
+		int totalEmpHours = 0;
+
+		while (totalEmpHours <= totalHours && totalWorkingDays < totalDays) {
+			totalWorkingDays++;
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+			System.out.println("Random Integers: " + empCheck);
+			switch (empCheck) {
+			case 1:
+				employeeHour = 8;
+				System.out.print(" Present");
+				break;
+			case 2:
+				employeeHour = 4;
+				System.out.print("Part time Present ");
+				break;
+			case 0:
+				employeeHour = 0;
+				System.out.println("Absent");
+				break;
+			}
+			empWage = employeeHour * wagePerHours;
+			totalEmpWage = totalEmpWage + empWage;
+			System.out.println("Day " + totalWorkingDays + " empHours :" + employeeHour);
+			System.out.println(" Wage :" + totalEmpWage);
+			System.out.println(" Total Wage :" + totalEmpWage);
+		}
+	}
 
 	public static void main(String[] args) {
 		EmployeeWage object = new EmployeeWage();
@@ -69,6 +102,7 @@ public class EmployeeWage {
 		object.employeePartTimeWage();
 		object.bySwitchCase();
 		object.monthlyWage();
+		object.calculateWagesTillGivenCond();
 	}
 
 }
