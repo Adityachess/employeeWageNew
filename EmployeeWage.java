a9dd95ca2704756fed02774e02341a9aea890da6
Adityachess <adityadone9398@gmail.com>
2021-10-18 12:30:12 +0530

    Added:Check attendance of employeeby using switch case.

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,7 @@
 package com.bz.employeewage;
 
+import java.util.Random;
+
 public class EmployeeWage {
 
 	int wagePerHours = 20;
@@ -7,6 +9,8 @@ public class EmployeeWage {
 	int dailyWage;
 	int partTimeWage;
 	int partTimeHours = 4;
+	int employeeHour;
+
 
 	public void employeeAttdence() {
 		int empCheck = (int) (Math.random() * 10) % 2;
@@ -28,13 +32,33 @@ public class EmployeeWage {
 		partTimeWage = wagePerHours * partTimeHours;
 		System.out.println("Employee Part Time Wage :" + partTimeWage);
 	}
+	
+	public void bySwitchCase() {
+		Random ran = new Random();
+		int empCheck = ran.nextInt(3);
+		switch (empCheck) {
+		case 0:
+			employeeHour = 8;
+			System.out.print("Present");
+			break;
+		case 1:
+			employeeHour = 4;
+			System.out.print("Part Time");
+			break;
+		case 2:
+			employeeHour = 0;
+			System.out.println("Absent");
+			break;
+		}
+	}
+
 
 	public static void main(String[] args) {
 		EmployeeWage object = new EmployeeWage();
 		object.employeeAttdence();
 		object.dailyWage();
 		object.employeePartTimeWage();
-
+		object.bySwitchCase();
 	}
 
 }
